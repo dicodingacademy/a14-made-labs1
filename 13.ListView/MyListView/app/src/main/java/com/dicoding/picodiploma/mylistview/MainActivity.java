296423223d21e30c62954b284d293d17ab9233ba
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2019-01-22 09:40:49 +0700

    Update MyListView and Optimize Code

--- 13.ListView/MyListView/app/src/main/java/com/dicoding/picodiploma/mylistview/MainActivity.java
+++ 13.ListView/MyListView/app/src/main/java/com/dicoding/picodiploma/mylistview/MainActivity.java
@@ -1,8 +1,8 @@
 package com.dicoding.picodiploma.mylistview;
 
 import android.content.res.TypedArray;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -12,39 +12,45 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[]data_nama;
-    String[]data_deskripsi;
-    TypedArray image_pahlawan;
+    String[] dataName;
+    String[] dataDescription;
+    TypedArray imagePahlawan;
     PahlawanAdapter adapter;
     ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.lv_list);
-        data_nama = getResources().getStringArray(R.array.data_nama);
-        data_deskripsi = getResources().getStringArray(R.array.data_deskripsi);
-        image_pahlawan = getResources().obtainTypedArray(R.array.data_foto);
+
+        prepare();
+
+        addItem();
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, data_nama[i], Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, dataName[i], Toast.LENGTH_SHORT).show();
             }
         });
-
-        addItem();
     }
 
-    public void addItem(){
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        imagePahlawan = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
         ArrayList<Pahlawan> pahlawan = new ArrayList<>();
-        adapter = new PahlawanAdapter(this,pahlawan);
+        adapter = new PahlawanAdapter(this, pahlawan);
 
-
-        for (int i=0;i<data_nama.length;i++){
+        for (int i = 0; i < dataName.length; i++) {
             Pahlawan pahlawans = new Pahlawan();
-            pahlawans.setImage(image_pahlawan.getResourceId(i,-1));
-            pahlawans.setNama(data_nama[i]);
-            pahlawans.setDeskripsi(data_deskripsi[i]);
+            pahlawans.setImage(imagePahlawan.getResourceId(i, -1));
+            pahlawans.setName(dataName[i]);
+            pahlawans.setDescription(dataDescription[i]);
             pahlawan.add(pahlawans);
             adapter.setPahlawans(pahlawan);
         }
---
+++ 13.ListView/MyListView/app/src/main/java/com/dicoding/picodiploma/mylistview/Pahlawan.java
@@ -0,0 +1,31 @@
+package com.dicoding.picodiploma.mylistview;
+
+public class Pahlawan {
+
+    int image;
+    String name, description;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
