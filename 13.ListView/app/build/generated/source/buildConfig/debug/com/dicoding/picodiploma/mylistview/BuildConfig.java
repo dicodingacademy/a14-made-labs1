292135f02f8d77921579432b09145b7f2fe78580
IanLaFlair <luzifer838@gmail.com>
2019-01-22 14:06:37 +0700

    Optimize Code ListView

---
+++ 13.ListView/app/build/generated/source/buildConfig/debug/com/dicoding/picodiploma/mylistview/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dicoding.picodiploma.mylistview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dicoding.picodiploma.mylistview";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
