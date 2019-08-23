74aafdf625382aa3dcda108e3ab7496a7b92b227
Arif <faizin.arief@gmail.com>
2019-08-23 19:33:22 +0700

    Update MyUnitTest based on codacy

--- Testing/MyUnitTesting/java/app/src/main/java/com/dicoding/picodiploma/myunittesting/MainActivity.java
+++ Testing/MyUnitTesting/java/app/src/main/java/com/dicoding/picodiploma/myunittesting/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements MainView {
-    EditText edtWidth, edtHeight, edtLength;
-    TextView tvResult;
+    private EditText edtWidth, edtHeight, edtLength;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
