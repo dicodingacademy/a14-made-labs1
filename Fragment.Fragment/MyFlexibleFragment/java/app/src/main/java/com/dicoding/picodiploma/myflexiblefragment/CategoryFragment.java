87758b32e4fba06ffed0966ca5ec77f8a68944bf
Arif <faizin.arief@gmail.com>
2019-08-26 11:28:57 +0700

    Update Android X Fragment

--- Fragment.Fragment/MyFlexibleFragment/java/app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.java
+++ Fragment.Fragment/MyFlexibleFragment/java/app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.java
@@ -2,11 +2,11 @@ package com.dicoding.picodiploma.myflexiblefragment;
 
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- Fragment.Fragment/MyFlexibleFragment/java/app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/ProfileActivity.java
+++ Fragment.Fragment/MyFlexibleFragment/java/app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/ProfileActivity.java
@@ -1,7 +1,7 @@
 package com.dicoding.picodiploma.myflexiblefragment;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
