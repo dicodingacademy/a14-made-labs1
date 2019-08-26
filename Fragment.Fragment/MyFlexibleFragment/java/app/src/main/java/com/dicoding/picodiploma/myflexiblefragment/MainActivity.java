114e32aaa3d175e27338f4b520ab6cc57e7618d6
Arif <faizin.arief@gmail.com>
2019-08-26 11:28:57 +0700

    Update Android X Fragment

--- Fragment.Fragment/MyFlexibleFragment/java/app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/MainActivity.java
+++ Fragment.Fragment/MyFlexibleFragment/java/app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/MainActivity.java
@@ -1,10 +1,10 @@
 package com.dicoding.picodiploma.myflexiblefragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
