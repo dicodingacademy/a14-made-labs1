5492c2d70ae3c983ba08fdd743cf91379f23d84e
Arif <faizin.arief@gmail.com>
2019-08-26 11:38:04 +0700

    Update Java Android X MyIntentApp

--- Intent/MyIntentApp/java/app/src/main/java/com/dicoding/picodiploma/myintentapp/MoveWithDataActivity.java
+++ Intent/MyIntentApp/java/app/src/main/java/com/dicoding/picodiploma/myintentapp/MoveWithDataActivity.java
@@ -1,7 +1,7 @@
 package com.dicoding.picodiploma.myintentapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MoveWithDataActivity extends AppCompatActivity {
