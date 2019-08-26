8c5245ed7d7fcdd038f11eb66faab16412131748
Arif <faizin.arief@gmail.com>
2019-08-26 11:44:39 +0700

    Update Java Android X MyListView

--- ListView/MyListView/java/app/src/main/java/com/dicoding/picodiploma/mylistview/MainActivity.java
+++ ListView/MyListView/java/app/src/main/java/com/dicoding/picodiploma/mylistview/MainActivity.java
@@ -2,7 +2,7 @@ package com.dicoding.picodiploma.mylistview;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
