21446541a95e59cfa6e5d0b70ffa76aedf7441d8
Arif <faizin.arief@gmail.com>
2019-08-26 11:51:45 +0700

    Update Java Android X MyTestingApp

--- Testing/MyTestingApp/java/app/src/main/java/com/dicoding/picodiploma/mytestingapp/MainActivity.java
+++ Testing/MyTestingApp/java/app/src/main/java/com/dicoding/picodiploma/mytestingapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.dicoding.picodiploma.mytestingapp;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
