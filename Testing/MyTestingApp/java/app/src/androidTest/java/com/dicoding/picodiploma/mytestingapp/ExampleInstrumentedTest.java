4ade8e8af2048218c95ca5002078b6cfadaf78bd
Arif <faizin.arief@gmail.com>
2019-08-26 16:55:59 +0700

    Remove Unused import

--- Testing/MyTestingApp/java/app/src/androidTest/java/com/dicoding/picodiploma/mytestingapp/ExampleInstrumentedTest.java
+++ Testing/MyTestingApp/java/app/src/androidTest/java/com/dicoding/picodiploma/mytestingapp/ExampleInstrumentedTest.java
@@ -4,7 +4,6 @@ import android.content.Context;
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
