f58af02f62de5ad039d93375209610a82585cce1
Dimas Catur <dimascatur48@gmail.com>
2020-11-18 10:37:07 +0700

    Update to Android Studio 4.1

--- Intent/MyIntentApp/java/app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.java
+++ Intent/MyIntentApp/java/app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.java
@@ -3,12 +3,11 @@ package com.dicoding.picodiploma.myintentapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView tvResult;
